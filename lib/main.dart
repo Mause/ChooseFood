0b3cd34182df09d11dd30ea0b6642f86cf512ca0
Elliana May <me@mause.me>
2021-10-29 12:49:41 +0800

    Simplify cards

--- lib/main.dart
+++ lib/main.dart
@@ -205,19 +205,18 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
 
-    var locations = results.sublist(0, min(results.length, 10)).map(
-          (e) => Card(
-            elevation: 5,
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  child: Image.network(e.photos[0].photoReference),
-                ),
-                Text(e.name),
-              ],
+    var locations = results.map(
+      (e) => Card(
+        child: Column(
+          children: [
+            Expanded(
+              child: Image.network(e.photos[0].photoReference),
             ),
-          ),
-        );
+            Text(e.name),
+          ],
+        ),
+      ),
+    );
 
     return BasePage(
       selectedIndex: 0,
