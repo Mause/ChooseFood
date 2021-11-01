72efb117c1e4d19f36828f4b6946f6d3be9dca24
Elliana May <me@mause.me>
2021-11-01 02:08:47 +0000

    Remove unused imports

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,15 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'dart:io';
-
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:choose_food/main.dart';
-import 'package:integration_test/integration_test.dart'
-    show IntegrationTestWidgetsFlutterBinding;
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
