74bbddf74ce43744d4d9344886a229e3d8a7934a
Elliana <me@mause.me>
2021-11-03 23:49:38 +0800

    Rewrite to use nock for http mocking

--- integration_test/screenshot_test.dart
+++ integration_test/screenshot_test.dart
@@ -1,36 +1,30 @@
-import 'dart:io'
-    show HttpClient, HttpClientRequest, HttpClientResponse, HttpOverrides, SecurityContext;
-
-import 'package:choose_food/main.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:get/get.dart';
-import 'package:integration_test/integration_test.dart';
-import 'package:mockito/mockito.dart';
-import 'package:supabase/supabase.dart';
-
-class MockHttpClient extends Mock implements HttpClient {}
-
-class MockHttpClientRequest extends Mock implements HttpClientRequest {}
-
-class MockHttpClientResponse extends Mock implements HttpClientResponse {}
-
-class MyHttpOverrides extends HttpOverrides {
-  HttpClient httpClient;
-  MyHttpOverrides(this.httpClient);
-  @override
-  HttpClient createHttpClient(SecurityContext? sc) => httpClient;
-}
+import 'package:choose_food/main.dart' show MyApp;
+import 'package:flutter_test/flutter_test.dart'
+    show WidgetTester, find, setUp, tearDown, testWidgets;
+import 'package:get/get.dart' show Get, Inst;
+import 'package:integration_test/integration_test.dart'
+    show IntegrationTestWidgetsFlutterBinding;
+import 'package:nock/nock.dart' show nock;
+import 'package:supabase/supabase.dart' show SupabaseClient;
 
 void main() {
   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized()
       as IntegrationTestWidgetsFlutterBinding;
 
+  setUp(() {
+    nock.init();
+  });
+  tearDown(() {
+    nock.cleanAll();
+  });
+
   testWidgets('screenshot', (WidgetTester tester) async {
     await Get.deleteAll(force: true);
     Get.put(SupabaseClient("https://dummy", "dummy"), permanent: true);
-    var mockHttpClient = MockHttpClient();
-    throwOnMissingStub(mockHttpClient);
-    HttpOverrides.global = MyHttpOverrides(mockHttpClient);
+
+    nock("https://dummy").get("/rest/v1/session?select=%2A").reply(200, [
+      {"id": "0000-00000-00000-00000"}
+    ]);
 
     // Build the app.
     await tester.pumpWidget(const MyApp());
@@ -42,15 +36,6 @@ void main() {
     await binding.takeScreenshot('screenshot-default');
 
     await tester.tap(find.text("Friends sessions"));
-
-    var mockHttpClientRequest = MockHttpClientRequest();
-    throwOnMissingStub(mockHttpClientRequest);
-    var mockHttpClientResponse = MockHttpClientResponse();
-    when(mockHttpClientResponse.first).thenReturn(Future.value("[]".codeUnits));
-    when(mockHttpClientRequest.done)
-        .thenReturn(Future.value(mockHttpClientResponse));
-    when(mockHttpClient.get("dummy", 443, "/rest/v1/session?select=%2A"))
-        .thenAnswer((_) => Future.value(mockHttpClientRequest));
 
     await tester.pumpAndSettle();
     await binding.takeScreenshot('screenshot-friends');
