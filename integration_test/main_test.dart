28efaeb49f4ea4cab25ea3aa23ea9639dbd34b90
Elliana May <me@mause.me>
2021-11-03 01:30:24 +0000

    Get closer to the example

--- integration_test/main_test.dart
+++ integration_test/main_test.dart
@@ -5,10 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'dart:io';
+import 'dart:io' show Platform;
 
 // import 'package:flutter/material.dart' show ElevatedButton, ListView;
-import 'package:flutter_test/flutter_test.dart' show testWidgets;
+import 'package:flutter_test/flutter_test.dart' show group, testWidgets;
 
 import 'package:choose_food/main.dart' show MyApp;
 import 'package:integration_test/integration_test.dart'
@@ -19,23 +19,25 @@ void main() {
       IntegrationTestWidgetsFlutterBinding.ensureInitialized()
           as IntegrationTestWidgetsFlutterBinding;
 
-  testWidgets('Places load', (tester) async {
-    tester.printToConsole("pumping");
-    await tester.pumpWidget(const MyApp());
-    tester.printToConsole("pumped");
+  group('ChooseFood', () {
+    testWidgets('Places load', (tester) async {
+      tester.printToConsole("pumping");
+      await tester.pumpWidget(const MyApp());
+      tester.printToConsole("pumped");
 
 //    await tester.tap(find.widgetWithText(ElevatedButton, 'Get places'));
 //    await tester.pump();
 
 //    expect(find.byType(ListView), findsOneWidget);
 
-    tester.printToConsole("converting");
-    await binding.convertFlutterSurfaceToImage();
-    tester.printToConsole("converted");
+      tester.printToConsole("converting");
+      await binding.convertFlutterSurfaceToImage();
+      tester.printToConsole("converted");
 
-    tester.printToConsole("screenshot");
-    await binding.callbackManager
-        .takeScreenshot('screenshot-$Platform.operatingSystem');
-    tester.printToConsole("screenshot");
+      tester.printToConsole("screenshot");
+      await binding.callbackManager
+          .takeScreenshot('screenshot-$Platform.operatingSystem');
+      tester.printToConsole("screenshot");
+    });
   });
 }
