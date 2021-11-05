5febe4568e4cca50a187d276d496016db584a91d
Elliana May <me@mause.me>
2021-11-05 01:43:29 +0000

    Add printCommunication

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,9 +1,14 @@
-import 'dart:io';
-import 'package:integration_test/integration_test_driver_extended.dart';
+import 'dart:io' show File;
+import 'package:flutter_driver/flutter_driver.dart' show FlutterDriver;
+import 'package:integration_test/integration_test_driver_extended.dart'
+    show integrationDriver;
 
 Future<void> main() async {
   try {
+    var driver = await FlutterDriver.connect(printCommunication: true);
+
     await integrationDriver(
+      driver: driver,
       onScreenshot: (String screenshotName, List<int> screenshotBytes) async {
         final File image = await File('screenshots/$screenshotName.png')
             .create(recursive: true);
