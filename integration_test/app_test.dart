f934f51a18bbc96287487806bc083fdc227fd7d8
Elliana May <me@mause.me>
2021-11-08 08:26:37 +0000

    Add demo files

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,6 +1,10 @@
 import 'dart:async';
 import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
+import 'hooks/hook_example.dart';
+import 'steps/colour_parameter.dart';
+import 'steps/given_i_pick_a_colour_step.dart';
+import 'steps/tap_button_n_times_step.dart';
 
 Future<void> main() {
   final config = FlutterTestConfiguration()
@@ -9,9 +13,12 @@ Future<void> main() {
       ProgressReporter(),
       TestRunSummaryReporter(),
       JsonReporter(path: './report.json')
-    ]
+    ] // you can include the "StdoutReporter()" without the message level parameter for verbose log information
+    ..hooks = [HookExample()]
+    ..stepDefinitions = [TapButtonNTimesStep(), GivenIPickAColour()]
+    ..customStepParameterDefinitions = [ColourParameter()]
     ..restartAppBetweenScenarios = true
-    ..targetAppPath = "test_driver/integration_test.dart";
+    ..targetAppPath = "test_driver/app.dart";
   // ..tagExpression = "@smoke" // uncomment to see an example of running scenarios based on tag expressions
   return GherkinRunner().execute(config);
 }
