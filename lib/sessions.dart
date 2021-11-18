3d55f559654f8f0dffa5dc957c6a7462380247b0
Elliana May <me@mause.me>
2021-11-18 15:34:34 +0800

    Add model

--- lib/sessions.dart
+++ lib/sessions.dart
@@ -1,8 +1,13 @@
 import 'package:choose_food/main.dart';
 import 'package:get/get.dart';
 import 'package:supabase/supabase.dart' show SupabaseClient, User;
+import 'package:json_annotation/json_annotation.dart';
 import 'common.dart';
 import 'generated_code/openapi.models.swagger.dart' show Participant, Session;
+import 'generated_code/openapi.models.swagger.dart'
+    show Decision, Participant, Session;
+
+part 'sessions.g.dart';
 
 class Sessions {
   SupabaseClient supabaseClient = Get.find();
@@ -50,4 +55,24 @@ class Sessions {
 
     return agreements;
   }
+}
+
+@JsonSerializable()
+class ParticipantWithDecisions extends Participant {
+  final List<Decision> decision;
+
+  ParticipantWithDecisions(
+      {required this.decision,
+      required String createdAt,
+      required int id,
+      required String sessionId,
+      required String userId})
+      : super(
+            createdAt: createdAt, id: id, sessionId: sessionId, userId: userId);
+
+  factory ParticipantWithDecisions.fromJson(Map<String, dynamic> json) =>
+      _$ParticipantWithDecisionsFromJson(json);
+
+  @override
+  toJson() => _$ParticipantWithDecisionsToJson(this);
 }
