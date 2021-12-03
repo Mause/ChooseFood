64558923dc9b5bafb8f6b0ec7475edc8c175da72
Elliana <me@mause.me>
2021-12-03 21:22:45 +0800

    refactor: Use GetNavigation in more places

--- lib/common/auth_required_state.dart
+++ lib/common/auth_required_state.dart
@@ -1,15 +1,16 @@
-import '../components/login_dialog.dart' show LoginDialog;
-import 'package:flutter/widgets.dart' show Navigator, StatefulWidget;
+import 'package:flutter/widgets.dart' show StatefulWidget;
+import 'package:get/get.dart' show Get, GetNavigation;
 import 'package:supabase_flutter/supabase_flutter.dart'
     show SupabaseAuthRequiredState;
+
+import '../components/login_dialog.dart' show LoginDialog;
 
 class AuthRequiredState<T extends StatefulWidget>
     extends SupabaseAuthRequiredState<T> {
   @override
   void onUnauthenticated() {
     if (mounted) {
-      Navigator.of(context)
-          .pushNamedAndRemoveUntil(LoginDialog.routeName, (route) => false);
+      Get.offNamedUntil(LoginDialog.routeName, (route) => false);
     }
   }
 }
